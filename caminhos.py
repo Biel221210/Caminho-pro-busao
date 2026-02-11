30abc907714824d69870e6eb4cffbd417d157dd1
Biel221210 <149751756+Biel221210@users.noreply.github.com>
2026-02-11 19:34:04 -0300

    Update caminhos.py

--- caminhos.py
+++ caminhos.py
@@ -4,10 +4,10 @@ import subprocess
 caminhos = random.choice([1, 2, 3, 4])
 
 if caminhos == 1:
-    subprocess.run(['cat', 'caminho1'])
+    subprocess.run(['cat', 'caminho1.txt'])
 elif caminhos == 2:
-    subprocess.run(['cat', 'caminho2'])
+    subprocess.run(['cat', 'caminho2.txt'])
 elif caminhos == 3:
-    subprocess.run(['cat', 'caminho3'])
+    subprocess.run(['cat', 'caminho3.txt'])
 elif caminhos == 4:
-    subprocess.run(['cat', 'caminho4'])
+    subprocess.run(['cat', 'caminho4.txt'])
